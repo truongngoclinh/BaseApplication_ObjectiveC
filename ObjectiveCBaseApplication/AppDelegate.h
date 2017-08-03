8f6b0ae3c50f9b551c1ccf7d8913d1f8278f9cc7
Linh <Linh@Truong.local>
2017-08-03 16:47:54 +0700

    Initial Commit

---
+++ ObjectiveCBaseApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjectiveCBaseApplication
+//
+//  Created by Linh on 8/3/17.
+//  Copyright © 2017 smvn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveCBaseApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjectiveCBaseApplication
+//
+//  Created by Linh on 8/3/17.
+//  Copyright © 2017 smvn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
